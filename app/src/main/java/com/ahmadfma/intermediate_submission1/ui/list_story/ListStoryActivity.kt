ada49a4ff486ccba0a4baa58045391cb9cceb7e7
ahmadfma <56568775+ahmadfma@users.noreply.github.com>
2022-04-13 19:00:03 +0800

    create activities

---
+++ app/src/main/java/com/ahmadfma/intermediate_submission1/ui/list_story/ListStoryActivity.kt
@@ -0,0 +1,12 @@
+package com.ahmadfma.intermediate_submission1.ui.list_story
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ahmadfma.intermediate_submission1.R
+
+class ListStoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_story)
+    }
+}
