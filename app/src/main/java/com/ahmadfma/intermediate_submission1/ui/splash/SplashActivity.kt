e522893f61f0feb2cee6208d4f56fb42970f1cfa
ahmadfma <56568775+ahmadfma@users.noreply.github.com>
2022-04-17 06:00:21 +0800

    check preferences in splash activity

--- app/src/main/java/com/ahmadfma/intermediate_submission1/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/ahmadfma/intermediate_submission1/ui/splash/SplashActivity.kt
@@ -1,14 +1,38 @@
 package com.ahmadfma.intermediate_submission1.ui.splash
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import androidx.lifecycle.lifecycleScope
 import com.ahmadfma.intermediate_submission1.R
+import com.ahmadfma.intermediate_submission1.data.local.UserPreferences
+import com.ahmadfma.intermediate_submission1.ui.login.LoginActivity
+import com.ahmadfma.intermediate_submission1.ui.main.MainActivity
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        lifecycleScope.launch {
+            delay(1500)
+            val intent = Intent()
+            val pref = UserPreferences(this@SplashActivity)
+            val token = pref.getUser().token
+            if(token != null && token.isNotEmpty()) {
+                intent.setClass(this@SplashActivity, MainActivity::class.java)
+            } else {
+                intent.setClass(this@SplashActivity, LoginActivity::class.java)
+            }
+            startActivity(intent)
+            finish()
+        }
     }
 }
