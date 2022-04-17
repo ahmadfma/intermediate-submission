c4effc7619d8e20da76b5d2994f69ee1b6346bca
ahmadfma <56568775+ahmadfma@users.noreply.github.com>
2022-04-17 15:49:07 +0800

    design add story layout

--- app/src/main/java/com/ahmadfma/intermediate_submission1/ui/add_story/AddStoryActivity.kt
+++ app/src/main/java/com/ahmadfma/intermediate_submission1/ui/add_story/AddStoryActivity.kt
@@ -2,11 +2,30 @@ package com.ahmadfma.intermediate_submission1.ui.add_story
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import com.ahmadfma.intermediate_submission1.R
+import com.ahmadfma.intermediate_submission1.databinding.ActivityAddStoryBinding
+import com.ahmadfma.intermediate_submission1.viewmodel.StoryViewModel
+import com.ahmadfma.intermediate_submission1.viewmodel.ViewModelFactory
 
 class AddStoryActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAddStoryBinding
+    private lateinit var viewModel: StoryViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_story)
+        binding = ActivityAddStoryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initVariable()
+        initListener()
+    }
+
+    private fun initVariable() {
+        viewModel = ViewModelProvider(this, ViewModelFactory.getInstance())[StoryViewModel::class.java]
+    }
+
+    private fun initListener() {
+
     }
 }
--- app/src/main/java/com/ahmadfma/intermediate_submission1/ui/main/MainActivity.kt
+++ app/src/main/java/com/ahmadfma/intermediate_submission1/ui/main/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.ahmadfma.intermediate_submission1.ui.main
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import com.ahmadfma.intermediate_submission1.R
 import com.ahmadfma.intermediate_submission1.databinding.ActivityMainBinding
+import com.ahmadfma.intermediate_submission1.ui.add_story.AddStoryActivity
 import com.ahmadfma.intermediate_submission1.ui.main.fragment.home.HomeFragment
 import com.ahmadfma.intermediate_submission1.ui.main.fragment.profile.ProfileFragment
 
@@ -34,7 +36,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         newStoryBtn.setOnClickListener {
-
+            startActivity(Intent(this@MainActivity, AddStoryActivity::class.java))
         }
     }
 
