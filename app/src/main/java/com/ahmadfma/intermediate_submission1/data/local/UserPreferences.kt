f8587324842c26a7d4247fdf7bbdeef83e4977d9
ahmadfma <56568775+ahmadfma@users.noreply.github.com>
2022-04-19 07:37:46 +0800

    fix UserPreferences

--- app/src/main/java/com/ahmadfma/intermediate_submission1/data/local/UserPreferences.kt
+++ app/src/main/java/com/ahmadfma/intermediate_submission1/data/local/UserPreferences.kt
@@ -1,7 +1,6 @@
 package com.ahmadfma.intermediate_submission1.data.local
 
 import android.content.Context
-import android.util.Log
 import com.ahmadfma.intermediate_submission1.data.model.UserData
 
 class UserPreferences(context: Context) {
@@ -9,9 +8,7 @@ class UserPreferences(context: Context) {
     companion object {
         private const val USER_PREF = "user_pref"
         private const val USER_TOKEN = "user token"
-        private const val USER_ID = "user id"
         private const val USER_NAME = "user name"
-        private const val TAG = "UserPreferences"
         var user = UserData()
     }
 
@@ -20,20 +17,16 @@ class UserPreferences(context: Context) {
     fun setUser(data: UserData) {
         val editor = preferences.edit()
         editor.putString(USER_TOKEN, data.token)
-        editor.putString(USER_ID, data.userId)
         editor.putString(USER_NAME, data.name)
         editor.apply()
         user = data
-        Log.d(TAG, "setUser: $data")
     }
 
     fun getUser(): UserData {
         val data = UserData()
         data.name = preferences.getString(USER_NAME, "")
         data.token = preferences.getString(USER_TOKEN, "")
-        data.userId = preferences.getString(USER_ID, "")
         user = data
-        Log.d(TAG, "getUser: $data")
         return data
     }
 
